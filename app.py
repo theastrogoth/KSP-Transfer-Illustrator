d15a1b20871afacf49129e94a6468c9c80495949
theastrogoth <52220914+theastrogoth@users.noreply.github.com>
2020-07-06 00:17:53 -0500

    Decreased porkchop sampling

--- app.py
+++ app.py
@@ -732,7 +732,7 @@ def update_porkchop_data(nClicks, system, dateFormat,
     # prepare porkchop table
     porkTable = PorkchopTable(sOrb, eOrb, transferType, noInsertion,
                               None, minStartTime, maxStartTime, 
-                              minFlightTime, maxFlightTime)
+                              minFlightTime, maxFlightTime, 51, 51)
     
     return jsonpickle.encode(porkTable)
 
