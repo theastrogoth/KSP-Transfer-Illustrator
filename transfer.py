2fd780c7026895f5facd19dfdce5b96a18ba4a0d
theastrogoth <52220914+theastrogoth@users.noreply.github.com>
2020-08-07 23:48:42 -0500

    Reduced max gens in genetic algorithm for speed

--- transfer.py
+++ transfer.py
@@ -821,7 +821,7 @@ class Transfer:
         return vec/norm(vec)
     
     
-    def genetic_refine(self, num = 10, tol = 100, maxGen = 200):
+    def genetic_refine(self, num = 10, tol = 1000, maxGen = 100):
         """Genetic algorithm to find start and end positions for Transfer"""
         
         # TO DO: figure out better mutation methods, convergence for high
